a782f4329b68af61918638f1cd38ceb97c08afa6
Robben28 <2371999072@qq.com>
2023-05-01 23:51:00 +1200

    Epochs bug

--- code/LSTM-baseline/Baseline-LSTM-MSELoss.py
+++ code/LSTM-baseline/Baseline-LSTM-MSELoss.py
@@ -189,7 +189,7 @@ train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate
 test_dataloader = DataLoader(test_dataset, batch_size=4, shuffle=True, collate_fn=collote_fn, drop_last=True)
 
 # Initialization model
-model = LSTM(512, 64, 5, 5, 4, bidirectional=False).to(device)
+model = LSTM(512, 1024, 5, 5, 4, bidirectional=False).to(device)
 
 # Use the cross entropy loss function
 loss_function = nn.MSELoss()
@@ -197,8 +197,8 @@ loss_function = nn.MSELoss()
 # Adam optimization function is used to define the learning rate
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-train_epochs = 5
-test_epochs = 1
+train_epochs = 50
+test_epochs = 10
 
 # train model
 loss,accurency = train_model(model, train_epochs, train_dataloader)
@@ -211,12 +211,12 @@ y_true = sorted([i for item in y_true for i in item])
 y_true = list(map(int,y_true))
 
 # visualization
-train_accurency = go.Scatter(y=accurency, name="accurency")
+train_accurency = go.Scatter(y=accurency, name="accuracy")
 trace_loss = go.Scatter(y=loss, name="loss")
-fig1 = go.Figure(data=[train_accurency, trace_loss], layout=go.Layout(title='loss/accurency-epoch', xaxis=dict(title='X轴'), yaxis=dict(title='Y轴')))
+fig1 = go.Figure(data=[train_accurency, trace_loss], layout=go.Layout(title='loss/accuracy-epoch', xaxis=dict(title='X轴'), yaxis=dict(title='Y轴')))
 
-test_accurency = go.Scatter(y=test_accurency, name='test accurency')
-fig2 = go.Figure(data=[test_accurency], layout=go.Layout(title='test accurency', xaxis=dict(title='X轴'), yaxis=dict(title='Y轴')))
+test_accurency = go.Scatter(y=test_accurency, name='test accuracy')
+fig2 = go.Figure(data=[test_accurency], layout=go.Layout(title='test accuracy', xaxis=dict(title='X轴'), yaxis=dict(title='Y轴')))
 
 trace_true = go.Scatter(y=y_true, name='y_true')
 fig3 = go.Figure(data=[trace_true], layout=go.Layout(title='y_true', xaxis=dict(title='X轴'), yaxis=dict(title='Y轴')))
@@ -224,7 +224,7 @@ fig3 = go.Figure(data=[trace_true], layout=go.Layout(title='y_true', xaxis=dict(
 ftrace_pred = go.Scatter(y=y_pred, name='y_pred')
 fig4 = go.Figure(data=[ftrace_pred], layout=go.Layout(title='y_pred', xaxis=dict(title='X轴'), yaxis=dict(title='Y轴')))
 
-fig = make_subplots(rows=2, cols=2, subplot_titles=('loss/accurency~epoch', 'test accurency~epoch', 'y_true', 'y_pred'))
+fig = make_subplots(rows=2, cols=2, subplot_titles=('loss/accuracy~epoch', 'test accuracy~epoch', 'y_true', 'y_pred'))
 fig.add_trace(fig1.data[0], row=1, col=1)
 fig.add_trace(fig1.data[1], row=1, col=1)
 fig.add_trace(fig2.data[0], row=1, col=2)
